ce97107a4bc31214a4036a40031ce00ccb603bca
Vel Gee <velmurugan.gandhirajm@gmail.com>
2025-05-31 13:01:04 +0530

    script for calculating simple interest is written

---
+++ python/simple_interest.py
@@ -0,0 +1,13 @@
+# Simple Python script to calculate simple interest
+
+# Input two integers
+
+p = int(input("Enter the amount you got"))
+n = int(input("Eneter number of months"))
+r = int(input("Enter rate of Interest"))
+
+#calculate
+
+SI = p*n*r/100
+
+print ("Interest is:", SI)
