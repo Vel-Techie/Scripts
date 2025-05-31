bb547f6df3e1d70b3b2d949fc4f5a6607cd26f58
Vel Gee <velmurugan.gandhirajm@gmail.com>
2025-05-31 12:47:35 +0530

    script for adding 2 integers is written

---
+++ python/Sum_2_int.py
@@ -0,0 +1,12 @@
+# Simple Python script to add two integers
+
+# Input two integers
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+
+# Add the numbers
+sum = num1 + num2
+
+# Display the result
+print("The sum is:", sum)
+
